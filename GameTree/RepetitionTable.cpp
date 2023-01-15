ac40384007ea633bcbae64c19b4c68e899833129
Stephen Bensley <76857027+stephenbensley@users.noreply.github.com>
2023-01-15 12:16:07 -0800

    Add RepetitionTableTest

--- GameTree/RepetitionTable.cpp
+++ GameTree/RepetitionTable.cpp
@@ -1,3 +1,10 @@
+//
+// Copyright 2023 Stephen E. Bensley
+//
+// This file is licensed under the MIT License. You may obtain a copy of the
+// license at https://github.com/stephenbensley/obatgonu/blob/main/LICENSE.
+//
+
 #include "RepetitionTable.h"
 #include "Graph.h"
 
--- GameTree/RepetitionTable.h
+++ GameTree/RepetitionTable.h
@@ -1,3 +1,10 @@
+//
+// Copyright 2023 Stephen E. Bensley
+//
+// This file is licensed under the MIT License. You may obtain a copy of the
+// license at https://github.com/stephenbensley/obatgonu/blob/main/LICENSE.
+//
+
 #ifndef RepetitionTable_h
 #define RepetitionTable_h
 
@@ -6,11 +13,15 @@
 class Graph;
 class Node;
 
+// Used to detect repetitions in a sequence of play.
 class RepetitionTable
 {
 public:
    RepetitionTable(const Graph& graph) noexcept;
+   // If the node has already been visited, returns false and does not push
+   // the node onto the stack. If this function succeeds, you must call pop.
    bool try_push(const Node& node) noexcept;
+   // Undoes a successful call to try_push.
    void pop(const Node& node) noexcept;
 
 private:
