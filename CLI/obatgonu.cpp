613dee1a8733b957bcb86c1e187947f8ed59071e
Stephen Bensley <76857027+stephenbensley@users.noreply.github.com>
2023-01-17 11:48:16 -0800

    Add CLI to dump self-play to stdout.

--- CLI/obatgonu.cpp
+++ CLI/obatgonu.cpp
@@ -6,13 +6,46 @@
 //
 
 #include "Graph.h"
+#include "MiniMax.h"
 #include "ToString.h"
 #include <iostream>
 
 int main(int argc, char* const argv[])
 {
-   Board board(5, 5);
-   Graph graph(board, 0b10001'11111);
-   std::cout << to_string(board, graph.start().position(board)) << std::flush;
+   // Build the game.
+   Board board(3, 3);
+   Graph graph(board, 0b111);
+   MiniMax solver(graph);
+
+   // Initial state.
+   auto node = graph.start();
+   auto pos = node.position(board);
+   auto player = node.player();
+   auto move_count = 0;
+
+   // Display the starting board.
+   std::cout << "Start" << std::endl;
+   std::cout << to_string(board, pos) << std::endl;
+
+   while (!node.is_terminal() && move_count < 100) {
+      // Calculate the next move.
+      auto next_node = solver.best_move(node, 100 - move_count);
+      auto next_pos = next_node.position(board);
+
+      // Output the result.
+      std::cout << "Player " << player + 1 << ": "
+                << to_string(board, pos[player], next_pos[player]);
+      if (next_node.is_terminal()) {
+         std::cout << "!";
+      }
+      std::cout << '\n' << to_string(board, next_pos) << std::endl;
+
+      // Update state.
+      node = next_node;
+      pos = next_pos;
+      player = node.player();
+      ++move_count;
+   }
+
    return 0;
 }
